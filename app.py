c3fbe43da7f939b7835877c964d6b967636f1197
Vishruth <vishruth24@gmail.com>
2018-04-01 01:34:00 +0530

    Compare module added

--- app.py
+++ app.py
@@ -17,6 +17,19 @@ def iphonex():
 def samsung():
     return redirect("http://www.samsung.com/global/galaxy/galaxy-s9/")
 
+@app.route('/brand',methods=['POST','GET'])
+def phone():
+    if request.method == "POST":
+        pass
+    else:
+        conn=sql.connect("static/phone.db")
+        c=conn.cursor()
+        c.execute('SELECT distinct(brand) FROM phones')
+        brand = c.fetchall()
+        c.close
+        conn.close()
+        return render_template('brands.html',brand=brand)
+
 @app.route('/brand/<brand_name>')
 def gen(brand_name):
     conn=sql.connect("static/phone.db")
@@ -25,12 +38,69 @@ def gen(brand_name):
     images = c.fetchall()
     c.close
     conn.close()
-    return render_template("generic.html",image=images,name=brand_name)
+    return render_template("phones.html",image=images,name=brand_name)
 
-@app.route('/lol')
-def lol():
-    return render_template('generic.html')
+@app.route('/brand/<brand_name>/<pname>')
+def details(brand_name,pname):
+    conn=sql.connect("static/phone.db")
+    c=conn.cursor()
+    c.execute('SELECT * FROM phones where brand=(?) and model=(?)',[brand_name,pname])
+    stuff = c.fetchall()
+    c.close
+    conn.close()
 
+    return render_template('detials.html',name=pname,data=stuff)
+
+@app.route('/compare/<m1>/<m2>')
+def compare(m1,m2):
+    conn=sql.connect("static/phone.db")
+    c=conn.cursor()
+    c.execute('SELECT * FROM phones where model=(?)',[m1])
+    model1 = c.fetchall()
+    c.execute('SELECT * FROM phones where model=(?)',[m2])
+    model2 = c.fetchall()
+
+    c.close
+    conn.close()
+
+    return render_template('compare.html',data1=model1,data2=model2)
+
+@app.route('/compare',methods=["post",'GET'])
+def comp():
+
+    if request.method == "POST":
+        b1=request.form['comp1']
+        b2=request.form['comp2']
+        conn=sql.connect("static/phone.db")
+        c=conn.cursor()
+        c.execute('SELECT model FROM phones where brand=(?)',[b1])
+        m1 = c.fetchall()
+        c.execute('SELECT model FROM phones where brand=(?)',[b2])
+        m2 = c.fetchall()
+
+        c.close
+        conn.close()
+        return render_template("bphone.html",m1=m1,m2=m2)
+
+
+    else:
+        conn=sql.connect("static/phone.db")
+        c=conn.cursor()
+        c.execute('SELECT distinct(brand) FROM phones')
+        brand = c.fetchall()
+        c.close
+        conn.close()
+        return render_template('comp.html',data=brand)
+
+@app.route('/useless',methods=["POST","GET"])
+def useless():
+    if request.method=="POST":
+        m1=request.form['p1']
+        m2=request.form['p2']
+
+        return redirect("compare/{}/{}".format(m1,m2))
+    else:
+        return "Hi there! Hope youre having a great day"
 
 if __name__ == '__main__':
   app.run(host='127.0.0.1', port=8000, debug=True)
